08f2d9988a233e78a01be6a5ea80be414aeb9675
DeedleFake <deedlefake@users.noreply.github.com>
2015-10-01 13:16:36 -0400

    Godoc and cleanup.

--- aur/client.go
+++ aur/client.go
@@ -7,21 +7,24 @@ import (
 	"net/url"
 )
 
-var (
+const (
+	// The default URL of the AUR.
 	AURURL = "https://aur.archlinux.org"
 )
 
+// Client provides an interface for accessing the AUR.
 type Client struct {
 	Client *http.Client
-	URL    string
+	RPC    string
 }
 
+// DefaultClient is a Client with default values.
 var DefaultClient = &Client{
 	Client: http.DefaultClient,
-	URL:    AURURL + "/rpc.php",
+	RPC:    AURURL + "/rpc.php",
 }
 
-func (c *Client) buildURL(t string, a ...string) string {
+func (c *Client) buildRPC(t string, a ...string) string {
 	arg := "arg"
 	if len(a) > 1 {
 		arg = "arg[]"
@@ -33,11 +36,11 @@ func (c *Client) buildURL(t string, a ...string) string {
 		q.Add(arg, a)
 	}
 
-	return c.URL + "?" + q.Encode()
+	return c.RPC + "?" + q.Encode()
 }
 
 func (c *Client) get(data interface{}, t string, a ...string) error {
-	rsp, err := c.Client.Get(c.buildURL(t, a...))
+	rsp, err := c.Client.Get(c.buildRPC(t, a...))
 	if err != nil {
 		return err
 	}
@@ -72,15 +75,19 @@ func (c *Client) get(data interface{}, t string, a ...string) error {
 	return json.Unmarshal(raw, data)
 }
 
-func Search(name string) ([]*PkgInfo, error) {
-	return DefaultClient.Search(name)
+// Search calls DefaultClient.Search().
+func Search(arg string) ([]*PkgInfo, error) {
+	return DefaultClient.Search(arg)
 }
 
-func (c *Client) Search(name string) ([]*PkgInfo, error) {
+// Search searches the AUR for packages whose names or descriptions
+// contain arg. If no packages are found, it returns an empty slice
+// and nil.
+func (c *Client) Search(arg string) ([]*PkgInfo, error) {
 	var data struct {
 		Results []*PkgInfo `json:"results"`
 	}
-	err := c.get(&data, "search", name)
+	err := c.get(&data, "search", arg)
 	if err != nil {
 		return nil, err
 	}
@@ -88,10 +95,13 @@ func (c *Client) Search(name string) ([]*PkgInfo, error) {
 	return data.Results, nil
 }
 
-func Info(name string) (*PkgInfo, error) {
-	return DefaultClient.Info(name)
+// Info calls DefaultClient.Info().
+func Info(arg string) (*PkgInfo, error) {
+	return DefaultClient.Info(arg)
 }
 
+// Info retrieves information about one package from the AUR. If no
+// such package exists, it returns nil, nil.
 func (c *Client) Info(name string) (*PkgInfo, error) {
 	var data struct {
 		ResultCount int             `json:"resultcount"`
@@ -115,10 +125,15 @@ func (c *Client) Info(name string) (*PkgInfo, error) {
 	return &result, nil
 }
 
-func MultiInfo(names ...string) ([]*PkgInfo, error) {
-	return DefaultClient.MultiInfo(names...)
+// MultiInfo calls DefaultClient.MultiInfo().
+func MultiInfo(args ...string) ([]*PkgInfo, error) {
+	return DefaultClient.MultiInfo(args...)
 }
 
+// MultiInfo gets information about multiple packages from the AUR. If
+// a package doesn't exist, it is simply omitted from the returned
+// slice. If none of the packages exist, the returned slice will be
+// empty.
 func (c *Client) MultiInfo(names ...string) ([]*PkgInfo, error) {
 	var data struct {
 		Results []*PkgInfo `json:"results"`
@@ -131,10 +146,14 @@ func (c *Client) MultiInfo(names ...string) ([]*PkgInfo, error) {
 	return data.Results, nil
 }
 
-func MSearch(name string) ([]*PkgInfo, error) {
-	return DefaultClient.MSearch(name)
+// MSearch calls DefaultClient.MSearch().
+func MSearch(arg string) ([]*PkgInfo, error) {
+	return DefaultClient.MSearch(arg)
 }
 
+// MSearch searches the AUR for all packages that have the given
+// maintainer. If no such packages exist, it returns an empty slice
+// and nil.
 func (c *Client) MSearch(name string) ([]*PkgInfo, error) {
 	var data struct {
 		Results []*PkgInfo `json:"results"`
@@ -147,6 +166,7 @@ func (c *Client) MSearch(name string) ([]*PkgInfo, error) {
 	return data.Results, nil
 }
 
+// PkgInfo contains information about a package entry in the AUR.
 type PkgInfo struct {
 	ID             int64
 	Name           string
@@ -166,6 +186,14 @@ type PkgInfo struct {
 	Popularity     float64
 }
 
+// OutOfDate wraps the OutOfDate parameter returned by the AUR's RPC
+// mechanism. The AUR returns an int, so this method is a convience to
+// convert that to a bool.
+func (pkg PkgInfo) OutOfDate() bool {
+	return pkg.outOfDate != 0
+}
+
+// Error represents an error returned from an RPC call to the AUR.
 type Error string
 
 func (err Error) Error() string {
--- version.go
+++ version.go
@@ -10,22 +10,34 @@ var (
 	verRE = regexp.MustCompile(`([0-9]+:)?[a-zA-Z0-9._]+-[0-9]+`)
 )
 
+// Version is a Pacman package version string. For more information,
+// see https://wiki.archlinux.org/index.php/PKGBUILD#Version
 type Version string
 
+// Valid checks if the version string is valid.
 func (v Version) Valid() bool {
 	return verRE.MatchString(string(v))
 }
 
+// Epoch returns the epoch part of the package version. If the version
+// doesn't contain an epoch, it returns -1.
+//
+// Note: This method doesn't perform any checks. Call v.Valid() before
+// calling this if you're unsure if the version string is valid.
 func (v Version) Epoch() int {
 	parts := strings.SplitN(string(v), ":", 2)
 	if len(parts) <= 1 {
-		return 0
+		return -1
 	}
 
 	e, _ := strconv.ParseInt(parts[0], 10, 0)
 	return int(e)
 }
 
+// Pkgver returns the main version part of the version string.
+//
+// Note: This method doesn't perform any checks. Call v.Valid() before
+// calling this if you're unsure if the version string is valid.
 func (v Version) Pkgver() string {
 	parts := strings.SplitN(string(v), ":", 2)
 	if len(parts) > 1 {
@@ -37,10 +49,15 @@ func (v Version) Pkgver() string {
 	return parts[0]
 }
 
+// Pkgrel returns the package release number. If the version string
+// doesn't contain a release number, which is invalid, it returns -1.
+//
+// Note: This method doesn't perform any checks. Call v.Valid() before
+// calling this if you're unsure if the version string is valid.
 func (v Version) Pkgrel() int {
 	parts := strings.SplitN(string(v), "-", 2)
 	if len(parts) <= 1 {
-		return 1
+		return -1
 	}
 
 	r, _ := strconv.ParseInt(parts[1], 10, 0)
